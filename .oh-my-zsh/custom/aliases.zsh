9a07071f7b750fa9c88885df9dc44c0a1fd73574
Asia Hoe <coding@asiahoe.com>
2018-02-28 21:30:06 -0800

    Remove outdated aliases and alphabetize.

--- .oh-my-zsh/custom/aliases.zsh
+++ .oh-my-zsh/custom/aliases.zsh
@@ -1,26 +1,28 @@
 # Aliases are shorthand for a command or a series of commands.
 # Type the alias into the Terminal to use.
 
-# Cask + Harmony Themes
-# Keeps Cask up to date with local changes to harmony-themes.
-# Update the bower file to fetch the local repo first.
-alias hu="bower cache clean; bower update harmony; grunt run"
-# Add all changes and amend previous commit.
-alias gane="git add .; git commit --amend --no-edit"
-
-# Git Clean Up
-# Remove unused references and branches on remote.
-alias prune="git fetch --prune"
-# Remove local merged branches with no current changes.
-alias snip="git branch --merged | grep -v \"\*\" | xargs -n 1 git branch -d"
-# Remove all local branches.
-alias trim="git branch | grep -v \"\*\" | xargs -n 1 git branch -D"
-
-# Modify ZSH Config
-alias zshrc="atom ~/.oh-my-zsh"
+# Atom
+## Backup packages to a generated list in specified directory.
+alias atomp="apm list --installed --bare > .atom/packages.txt"
+## Install your packages from a generated list in a specified directory.
+alias atomi="apm install --packages-file .atom/packages.txt"
 
 # Finder
-# Display or hide hidden system files in Finder.
-# Thanks to Ian Lunn. http://ianlunn.co.uk/articles/quickly-showhide-hidden-files-mac-os-x-mavericks/
+## Display or hide hidden system files in Finder.
+## Thanks to Ian Lunn. http://ianlunn.co.uk/articles/quickly-showhide-hidden-files-mac-os-x-mavericks/
 alias showFiles="defaults write com.apple.finder AppleShowAllFiles YES; killall Finder /System/Library/CoreServices/Finder.app"
 alias hideFiles="defaults write com.apple.finder AppleShowAllFiles NO; killall Finder /System/Library/CoreServices/Finder.app"
+
+# Git
+## Add all changes and amend previous commit.
+alias amend="git add .; git commit --amend --no-edit"
+## Remove unused references and branches on remote.
+alias prune="git fetch --prune"
+## Remove local merged branches with no current changes.
+alias snip="git branch --merged | grep -v \"\*\" | xargs -n 1 git branch -d"
+## Remove all local branches.
+alias trim="git branch | grep -v \"\*\" | xargs -n 1 git branch -D"
+
+# ZSH
+## Modify local ZSH config in Atom.
+alias zshrc="atom ~/.oh-my-zsh"
